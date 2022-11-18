f8ef4b361511753194a92f5452220516ea685d2e
ThomasSAZERAT <thomas.sazerat@etu.unilim.fr>
2022-11-18 17:27:16 +0100

    manifest - try to connect to BDD

--- server/manifest.js
+++ server/manifest.js
@@ -47,9 +47,9 @@ module.exports = new Confidence.Store({
                         knex: {
                             client: 'mysql',
                             connection: {
-                                host: process.env.DB_HOST || '127.0.0.1',
+                                host: process.env.DB_HOST || '0.0.0.0',
                                 user: process.env.DB_USER || 'root',
-                                password: process.env.DB_PASSWORD || '2261',
+                                password: process.env.DB_PASSWORD || '1234',
                                 database: process.env.DB_DATABASE || 'hapi'
                             },
                             migrations: {
